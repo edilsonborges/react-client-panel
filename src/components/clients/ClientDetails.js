08e7463fa046144cec901e427f3624aaa1654c05
Edilson Borges <edilsonfb@gmail.com>
2019-02-10 23:51:17 -0200

    Client details connecting firebase

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -1,14 +1,49 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import Spinner from "../layout/Spinner";
 
 class ClientDetails extends Component {
   render() {
-    const { id } = this.props.match.params;
-    return (
-      <div>
-        <h1>Testing</h1>
-      </div>
-    );
+    const { client } = this.props;
+    console.log(client);
+    if (client) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/" className="btn btn-link">
+                <i className="fas fa-arrow-circle-left"> Back To Dashboard</i>
+              </Link>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="btn-group float-right">
+              <Link to={`/client/edit/${client.id}`} className="btn btn-dark">
+                Edit
+              </Link>
+              <button className="btn btn-danger">Delete</button>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <Spinner />;
+    }
   }
 }
 
-export default ClientDetails;
+export default compose(
+  firestoreConnect(props => [
+    {
+      collection: "clients",
+      storeAs: "client",
+      doc: props.match.params.id
+    }
+  ]),
+  connect(({ firestore: { ordered } }, props) => ({
+    client: ordered.client && ordered.client[0]
+  }))
+)(ClientDetails);
