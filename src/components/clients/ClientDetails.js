825596192c73571adac4d79895fd22b4add79d80
Edilson Borges <edilsonfb@gmail.com>
2019-02-12 23:48:36 -0200

    client details fixing bug, add condition for css balance

--- src/components/clients/ClientDetails.js
+++ src/components/clients/ClientDetails.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import { firestoreConnect } from "react-redux-firebase";
 import Spinner from "../layout/Spinner";
+import classnames from "classnames";
 
 class ClientDetails extends Component {
   render() {
@@ -14,18 +16,19 @@ class ClientDetails extends Component {
           <div className="row">
             <div className="col-md-6">
               <Link to="/" className="btn btn-link">
-                <i className="fas fa-arrow-circle-left"> Back To Dashboard</i>
+                <i className="fas fa-arrow-circle-left" /> Back To Dashboard
               </Link>
             </div>
-          </div>
-          <div className="col-md-6">
-            <div className="btn-group float-right">
-              <Link to={`/client/edit/${client.id}`} className="btn btn-dark">
-                Edit
-              </Link>
-              <button className="btn btn-danger">Delete</button>
+            <div className="col-md-6">
+              <div className="btn-group float-right">
+                <Link to={`/client/edit/${client.id}`} className="btn btn-dark">
+                  Edit
+                </Link>
+                <button className="btn btn-danger">Delete</button>
+              </div>
             </div>
           </div>
+          <hr />
           <div className="card">
             <h3 className="card-header">
               {client.firstName} {client.lastName}
@@ -40,10 +43,28 @@ class ClientDetails extends Component {
                 </div>
                 <div className="col-md-4 col-sm-6">
                   <h3 className="pull-right">
-                    Balance: ${parseFloat(client.balance).toFixed(2)}
+                    Balance:{" "}
+                    <span
+                      className={classnames({
+                        "text-danger": client.balance > 0,
+                        "text-success": client.balance == 0
+                      })}
+                    >
+                      ${parseFloat(client.balance).toFixed(2)}
+                    </span>
                   </h3>
+                  {/* @todo - balance form */}
                 </div>
               </div>
+              <hr />
+              <ul className="list-group">
+                <li className="list-group-item">
+                  Contact Email: {client.email}
+                </li>
+                <li className="list-group-item">
+                  Contact Phone: {client.phone}
+                </li>
+              </ul>
             </div>
           </div>
         </div>
@@ -53,6 +74,10 @@ class ClientDetails extends Component {
     }
   }
 }
+
+ClientDetails.propTypes = {
+  firestore: PropTypes.object.isRequired
+};
 
 export default compose(
   firestoreConnect(props => [
