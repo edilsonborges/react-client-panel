bb738ead26149f520a279410f79d0f698d23bd42
Edilson Borges <edilsonfb@gmail.com>
2019-03-06 12:04:22 -0300

    changes

---
+++ public/precache-manifest.8b9dec7b326e8b1f7e43cb4b34f913f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c7eb4e24a546899136b5",
+    "url": "/static/js/main.c7eb4e24.chunk.js"
+  },
+  {
+    "revision": "76a22b7248802450fde1",
+    "url": "/static/js/2.76a22b72.chunk.js"
+  },
+  {
+    "revision": "c7eb4e24a546899136b5",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "dbc31712be3ddc18fe5e59879611e4e6",
+    "url": "/index.html"
+  }
+];
